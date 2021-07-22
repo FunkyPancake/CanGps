5ecf20ced2485f2ed30adc1d6ad002ce130639b3
FunkyPancake <matdeb01685@gmail.com>
2021-07-22 02:30:17 +0200

    working nmea message disable and ubx output message enable

--- Application/NeoM9N/NeoM9N.cpp
+++ Application/NeoM9N/NeoM9N.cpp
@@ -9,36 +9,48 @@ void NeoM9N::Config()
 {
     {
         std::vector<uint8_t> data = UbxMessage(UbxMessage::CFG, 0x8a, {
-            0, 1, 0, 0x40, 0x52, 0x00, 0x01, 0, 0x07, 0x08, 0x00}).Serialize();
+            0x00, 0x01, 0x00,
+            0x40, 0x52, 0x00, 0x01,
+            0, 0x07, 0x08, 0x00
+        }).Serialize();
         _com->WriteBytes(data);
         auto a = _com->ReadBytes(10);
     }
     {
         std::vector<uint8_t> data = UbxMessage(UbxMessage::CFG, 0x8a, {
-            0, 1, 0, 0x10,0x73,0x00,0x01,0x01}).Serialize();
+            0x00, 0x01, 0x00, 0x00,
+            0x01, 0x00, 0x73, 0x10,
+            0x01
+        }).Serialize();
         _com->WriteBytes(data);
         auto a = _com->ReadBytes(10);
     }
     {
         std::vector<uint8_t> data = UbxMessage(UbxMessage::CFG, 0x8a, {
-            0, 1, 0, 0x10,0x74,0x00,0x01,0x01}).Serialize();
+            0x00, 0x01, 0x00, 0x00,
+            0x01, 0x00, 0x74, 0x10,
+            0x01
+        }).Serialize();
         _com->WriteBytes(data);
         auto a = _com->ReadBytes(10);
     }
     {
         std::vector<uint8_t> data = UbxMessage(UbxMessage::CFG, 0x8a, {
-            0, 1, 0, 0x10,0x74,0x00,0x02,0x00}).Serialize();
+            0x00, 0x01, 0x00, 0x00,
+            0x02, 0x00, 0x74, 0x10,
+            0x00
+        }).Serialize();
         _com->WriteBytes(data);
         auto a = _com->ReadBytes(10);
     }
-    _com->SetBaudrate(460800);
+//    _com->SetBaudrate(460800);
 }
 bool NeoM9N::GetData()
 {
     std::vector<uint8_t> data = UbxMessage(UbxMessage::NAV,7,{}).Serialize();
     _com->WriteBytes(data);
-    auto rdata = _com->ReadBytes(98);
-    UbxMessage().Deserialize(rdata);
+//    auto rdata = _com->ReadBytes(98);
+//    UbxMessage().Deserialize(rdata);
     return true;
 }
 
